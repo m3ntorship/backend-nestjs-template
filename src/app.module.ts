1e1f4c43bda82aefaaee136c230e098899dd18cd
Sahl Khalifa <sahl.abdelrahman@gmail.com>
2021-03-28 16:18:28 +0200

    add configuration + typeorm connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import configuration from './config/configuration';
+import ormConfig from './ormconfig';
 
+const evnVariable = process.env.NODE_ENV || 'development';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      // load different .env files based on runtime environment variable
+      envFilePath: [`.${evnVariable}.env`],
+      isGlobal: true,
+      load: [configuration],
+    }),
+    // TypeOrm
+    TypeOrmModule.forRoot(ormConfig()),
+  ],
   controllers: [],
   providers: [],
 })
