347daa81496d1fb29ad669e3ef50348a2f20ac8b
seifeldeen3388 <seifeldeen3388@gmail.com>
2021-04-02 01:03:13 +0200

    corrected clients service for better readability

--- src/clients/clients.service.ts
+++ src/clients/clients.service.ts
@@ -1,18 +1,9 @@
-import {
-  Inject,
-  Injectable,
-  InternalServerErrorException,
-} from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { AxiosStatic } from 'axios';
 
 @Injectable()
 export class ClientsService {
-  constructor(
-    private readonly configService: ConfigService,
-    @Inject('axios') private readonly axios: AxiosStatic,
-    @Inject('interceptors') private readonly interceptors: any,
-  ) {}
   private readonly clientsConfig = this.configService.get('clients');
 
   private readonly clients = {
@@ -33,17 +24,24 @@ export class ClientsService {
     }),
   };
 
+  constructor(
+    private readonly configService: ConfigService,
+    @Inject('axios') private readonly axios: AxiosStatic,
+    @Inject('interceptors') private readonly interceptors: any,
+  ) {
+    this.interceptors.addHeader(this.clients.postsClient);
+    this.interceptors.addHeader(this.clients.uploadClient);
+    this.interceptors.changeData(this.clients.postsClient);
+  }
+
   postsAPI = {
     foo: async () => {
-      this.interceptors.addHeader(this.clients.postsClient);
-      // this.interceptors.changeData(this.clients.postsClient);
       return this.clients.postsClient.get('/bar');
     },
   };
 
   uploadAPI = {
     foo: () => {
-      this.interceptors.addHeader(this.clients.uploadClient);
       return this.clients.uploadClient.post('/bar', { foo: 'bar' });
     },
   };
