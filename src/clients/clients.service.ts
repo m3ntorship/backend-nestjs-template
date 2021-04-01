f5a48e6ace19528fefab48d520d76743932bb448
seifeldeen3388 <seifeldeen3388@gmail.com>
2021-04-01 15:08:14 +0200

    added clients module with apis fro diff clients

--- src/clients/clients.service.ts
+++ src/clients/clients.service.ts
@@ -1,8 +1,51 @@
 import { HttpService, Injectable } from '@nestjs/common';
+import axios from 'axios';
 
 @Injectable()
 export class ClientsService {
-  constructor(private clients, private httpService: HttpService) {
-    this.clients = {};
-  }
+  constructor(private httpService: HttpService) {}
+  clients = {
+    postsClient: axios.create({
+      baseURL: '', // config
+    }),
+    notificationsClient: axios.create({
+      baseURL: '', // config
+    }),
+    uploadClient: axios.create({
+      baseURL: '', // config
+    }),
+    mediaClient: axios.create({
+      baseURL: '', // config
+    }),
+  };
+  postsAPI = {
+    // Example
+    getPosts: (authorization: string) => {
+      return this.clients.postsClient.get('/posts', {
+        headers: {
+          authorization,
+        },
+      });
+    },
+  };
+  notificationsAPI = {
+    // Example
+    getNotifications: (authorization: string) => {
+      return this.clients.notificationsClient.get('/notifications', {
+        headers: {
+          authorization,
+        },
+      });
+    },
+  };
+  uploadAPI = {
+    // Example
+    uploadFile: (authorization: string) => {
+      return this.clients.uploadClient.post('/files', {
+        headers: {
+          authorization,
+        },
+      });
+    },
+  };
 }
